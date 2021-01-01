daeaf91d0ce6d6f9b7bae94df9a5ec3d7f5eb5c9
Hagb Green <hagb_green@qq.com>
2021-01-01 11:25:46 +0800

    Automatically generate the strings table

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -2,5 +2,5 @@
 
 ## New strings
 
-If you want to submit new strings, please add it to both [DeBooxUpx.py](DeBooxUpx.py) and [`The Strings` subsection](README.md#the-strings) in [README.md](README.md).
+If you want to submit new strings, please add it to [DeBooxUpx.py](DeBooxUpx.py) and run [update\_readme\_strings.py](update_readme_strings.py) to update [the strings table](README.md#the-strings) in [README.md](README.md) (do not manually modify the table).
 
---
+++ update_readme_strings.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from DeBooxUpx import boox_strings
+import re
+model_width = 10
+
+with open('README.md') as readme:
+    readme_text = readme.read()
+models = sorted(boox_strings.items(), key=lambda x: x[0])
+table_header = f"""|{'': ^{model_width}}|{'MODEL': ^{model_width+2}}|{'STRING_SETTINGS': ^46}|{'STRING_UPGRADE': ^46}|{'STRING_LOCAL': ^42}|
+|{'-'*(model_width)}|{'-'*(model_width+2)}|{'-'*46}|{'-'*46}|{'-'*42}|
+"""
+table = table_header + '\n'.join(
+    f"|{name: ^{model_width}}|{'`'+string['MODEL']+'`': ^{model_width+2}}|`{string['STRING_SETTINGS']}`|`{string['STRING_UPGRADE']}`|`{string['STRING_LOCAL']}`|"
+    for name, string in models)
+with open('README.md', 'w') as readme:
+    readme.write(
+        re.sub(
+            r'<!--\(strings table begin\)-->.*<!--\(strings table end\)-->',
+            f'<!--(strings table begin)-->\n{table}\n<!--(strings table end)-->',
+            readme_text,
+            1,
+            flags=re.S))
