e9fb5034a9e2fdb3d17bc737f4ec891999368113
RenateUSB <35894241+RenateUSB@users.noreply.github.com>
2023-11-26 09:37:49 -0700

    Update BooxKeyConvert.py
    More explicit checks

--- BooxKeyConvert.py
+++ BooxKeyConvert.py
@@ -13,17 +13,30 @@ from base64 import b64decode
 from base64 import b64encode
 
 def decryptStr(tmpKey: bytes, string: str) -> str:
-    try:
-        cipher = DES.new(tmpKey, DES.MODE_CFB, iv=b'\xff' * 8, segment_size=64)
-        out: str = cipher.decrypt(b64decode(string)).decode()
-        if len(out) != 33 or out[32] != '\n': return None
-        return out[:32]
-    except:
+    if len(string) != 44:
+        print('Key is not the correct length of 44')
         return None
+    try:
+        data: bytes = b64decode(string)
+    except:
+        print('Key is not valid base64 encoding')
+        return None;
+    if len(data) != 33:
+        print('Base64 decoding of key returned length less than 33')
+        return None;
+    cypher = DES.new(tmpKey, DES.MODE_CFB, iv=b'\xff' * 8, segment_size=64)
+    data = cypher.decrypt(data)
+    try:
+        out: str = data.decode()
+    except:
+        print('Key did not decode correctly for this model')
+        return None
+    if out[32] != '\n': print('Decrypted key missing standard newline')
+    return out[:32]
 
 def encryptStr(tmpKey: bytes, string: str) -> str:
-    cipher = DES.new(tmpKey, DES.MODE_CFB, iv=b'\xff' * 8, segment_size=64)
-    out: str = b64encode(cipher.encrypt(bytearray(string.upper()+'\n', 'ascii'))).decode()
+    cypher = DES.new(tmpKey, DES.MODE_CFB, iv=b'\xff' * 8, segment_size=64)
+    out: str = b64encode(cypher.encrypt(bytearray(string.upper()+'\n', 'ascii'))).decode()
     return out
 
 if __name__ == '__main__':
@@ -34,12 +47,9 @@ if __name__ == '__main__':
             if i > 2: print()
             x = sys.argv[i]
             print(x)
-            if len(x) == 44:
-                y = decryptStr(tmpKey, x)
-                if y != None:
-                    print(y)
-                    z = encryptStr(tmpKey, y)
-                    if z != x: print('Key does not "round-trip" correctly')
-                else: print('Key does not decode correctly for this model')
-            else: print('Key is not the correct length of 44')
+            y = decryptStr(tmpKey, x)
+            if y != None:
+                print(y)
+                z = encryptStr(tmpKey, y)
+                if z != x: print('Key does not "round-trip" correctly')
     else: print('Usage: BooxKeyConvert.py <model> <settings> <upgrade>')
